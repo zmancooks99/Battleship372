b114f0ff97bee7fd4f1d5c52b0619123ba56e2dc
zamason <zamason@alaska.edu>
2020-02-09 13:43:48 -0900

    First commit of code

---
+++ cmake-build-debug/CMakeFiles/Battleship372.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Battleship372"
+  "Battleship372.pdb"
+  "CMakeFiles/Battleship372.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Battleship372.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Battleship372.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Battleship372.
+# This may be replaced when dependencies are built.
