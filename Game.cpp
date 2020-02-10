ef322a8d3c87eb5092c26a7bcb3719dd7705a017
zamason <zamason@alaska.edu>
2020-02-10 14:01:34 -0900

    Began adding functionality to Board class

--- Game.cpp
+++ Game.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "Game.h"
+
+Board::Board() : _b(_BOARD_DIM*_BOARD_DIM){
+
+}
+
+int Board::getLoc(int x, int y) {
+    return _b.at( ((--y)*_BOARD_DIM) + (--x));
+}
--- Model/Board.h
+++ Model/Board.h
@@ -8,7 +8,21 @@
 #include <vector>
 
 class Board {
+private:
+    const int _BOARD_DIM = 10;
+    const int _EMPTY = 0;
+    const int _SHIP = 1;
+    const int _MISS = 2;
+    const int _HIT = 3;
 
+    std::vector<int> _b;
+
+    int getLoc(int x, int y);
+public:
+    Board();
+
+    int at(int x, int y);
+    int set(int x, int y);
 };
 
 
