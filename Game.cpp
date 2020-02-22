2142fba04334ab0115ba6a0186aea2d972510041
zamason <zamason@alaska.edu>
2020-02-21 16:03:35 -0900

    Work done on Game -- Notes added to UI for Riley. A few getters for Board added as well

--- Game.cpp
+++ Game.cpp
@@ -7,7 +7,7 @@
 using std::vector;
 using std::pair;
 
-Game::Game() {
+Game::Game() : _gameOver(false){
     if(!_COMPUTER) {
         for (int i = 0; i < _NUM_PLAYERS; i++) {
             _boards.push_back(Board());
@@ -18,14 +18,32 @@ Game::Game() {
 }
 
 void Game::doTurn() {
-    for (int i = 0; i < _NUM_PLAYERS; i++) {
-        Board b = _boards.at(i);
-        UI u = _uis.at(i);
-        pair<int, int> shot = u.getShot();
-        while(!b.checkShot(shot.first, shot.second)) {
-            u.shotInvalid(shot);
-            shot = u.getShot();
+    while(!_gameOver) {
+        for (int i = 0; i < _NUM_PLAYERS; i++) {
+            Board b = _boards.at(i);
+            UI u = _uis.at(i);
+            pair<int, int> shot = u.getShot();
+            while (!b.checkShot(shot.first, shot.second)) {
+                u.shotInvalid(shot);
+                shot = u.getShot();
+            }
+            int r = b.addShot(shot.first, shot.second);
+            if (r == _HIT)
+                u.targetHit();
+            if (r == _MISS)
+                u.targetMiss();
+            if (r == _SUNK)
+                u.targetSunk();
+            u.printBoard(b.getBoardDim(), b.getBoard());
+            if (b.allSunk()) {
+                u.Win();
+                for(UI u1 : _uis) {
+                    if(&u != &u1)
+                        u1.Loss();
+                }
+                _gameOver = true;
+                break;
+            }
         }
-        b.addShot(shot.first, shot.second);
     }
 }
