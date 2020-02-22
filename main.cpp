d6d15ee44be22af7f4f7629e4bbf604e74b95a14
AlaskanGopher <revltess@gmail.com>
2020-02-21 15:17:03 -0900

    Fixed some UI and added a printing to Int main

--- main.cpp
+++ main.cpp
@@ -5,10 +5,19 @@
 #include "View/UI.h"
 
 int main() {
+    //------------PRINT BOARD-------------------
     UI test;
     int _boardLength = 10;
     std::vector<int> _boardArray(100);
     _boardArray.assign(100, 0);
     test.printBoard(_boardLength, _boardArray);
+    //-----------PRINT BOARD--------------------
+    //-------------All the UI Outputs-----------
+    test.Win();
+    test.Loss();
+    test.targetHit();
+    test.targetMiss();
+    test.targetSunk();
+    //-------------All the UI Outputs----------
     return 0;
 }
